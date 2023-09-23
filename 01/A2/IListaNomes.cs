f160ab2d57645a38ef572e70a15a1954d7c6c6b4
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-09-23 15:52:54 +0100

    feat(lab-01): add A2

---
+++ 01/A2/IListaNomes.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+public interface IListaNomes
+{
+    public void AddName(string name);
+    public string GetNames();
+    public void EraseNames();
+}
+
+public class NameManager : IListaNomes
+{
+    private List<string> names = new List<string>();
+
+    public void AddName(string name)
+    {
+        names.Add(name);
+    }
+
+    public string GetNames()
+    {
+        if (names.Count == 0)
+        {
+            return "Empty name list.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        foreach (string name in names)
+        {
+            sb.AppendLine(name);
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public void EraseNames()
+    {
+        names.Clear();
+    }
+}
---
+++ 01/A2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IListaNomes nameManager = new NameManager();
+
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add Name");
+            Console.WriteLine("2. List Names");
+            Console.WriteLine("3. Erase List");
+            Console.WriteLine("4. Quit");
+
+            Console.Write("Choose an option: ");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.Write("Type the name to be added: ");
+                    string nome = Console.ReadLine();
+                    nameManager.AddName(nome);
+                    Console.WriteLine($"Name '{nome}' was added to the list.");
+                    break;
+                case "2":
+                    Console.WriteLine("Names in the list:");
+                    Console.WriteLine(nameManager.GetNames());
+                    break;
+                case "3":
+                    nameManager.EraseNames();
+                    Console.WriteLine("The name's list has been cleared.");
+                    break;
+                case "4":
+                    Console.WriteLine("Quitting.");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
